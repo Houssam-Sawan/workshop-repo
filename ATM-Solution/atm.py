1a132d454897fdbee74897732afd795e37c751f4
Houssam Sawan <eng.houssam.sawan@gmail.com>
2018-02-15 12:06:18 +0300

    Refactor the ATM code as a function

--- ATM-Solution/atm.py
+++ ATM-Solution/atm.py
@@ -6,38 +6,47 @@ Output: dollar bills count
 '''
 
 # allowed papers: 100, 50, 10, 5, and rest of request
+def withdraw(balance, request):
 
-money = 500
+    print(f"Your current balance is: {balance}")
 
-request = 277
+    remaining_balance = balance
+    if request > balance:
+        print("Insufficient Fund ")
 
-if request > money:
-    print("Insufficient Fund ")
-    print(f"Your current balance is {money}")
+    elif request <= 0: print("The reqested amount must be positive")
 
-elif request <= 0: print("The reqested amount must be positive")
+    else:
 
-else:
+        remaining_balance = balance - request
 
-    while request > 0:
-        if request // 100 > 0:
-            print("Give 100")
-            request -= 100
+        while request > 0:
 
-        elif request // 50 > 0:
-            print("Give 50")
-            request -= 50
+            if request // 100 > 0:
+                print("Give 100")
+                request -= 100
 
-        elif request // 10 > 0:
-            print("Give 10")
-            request -= 10
+            elif request // 50 > 0:
+                print("Give 50")
+                request -= 50
 
-        elif request // 5 > 0:
-            print("Give 5")
-            request -= 5
+            elif request // 10 > 0:
+                print("Give 10")
+                request -= 10
 
-        elif request > 0:
-            print(f"Give {request}")
-            request = 0
+            elif request // 5 > 0:
+                print("Give 5")
+                request -= 5
 
-print("Done")
+            elif request > 0:
+                print(f"Give {request}")
+                request = 0
+
+    return remaining_balance
+
+balance = 500
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
