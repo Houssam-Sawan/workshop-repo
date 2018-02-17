ad8a18765d2d35bad8175e1b8355add1873cda5c
Houssam Sawan <eng.houssam.sawan@gmail.com>
2018-02-17 10:21:27 +0300

    Add a code to track and print widthrawals

--- ATM-Solution/atm.py
+++ ATM-Solution/atm.py
@@ -10,6 +10,7 @@ class ATM:
     def __init__(self, balance, bank_name):
         self.balance = balance
         self.bank_name =bank_name
+        self.withdrawals_list = []
 
 
     # allowed papers: 100, 50, 10, 5, and rest of request
@@ -28,6 +29,7 @@ class ATM:
 
         else:
 
+            self.withdrawals_list.append(request)
             remaining_balance = self.balance - request
 
             while request > 0:
@@ -53,18 +55,39 @@ class ATM:
                     request = 0
 
         print("===========================================")
-        
+
         self.balance = remaining_balance
         return remaining_balance
 
-balance1 = 500
-balance2 = 1000
+    def show_withdrawals(self):
+
+        print(f"The following amounts has been withdrawed from {self.bank_name}: ")
+        for withdrawal in self.withdrawals_list:
+            print(withdrawal)
+
+        print(f"Current balance = {self.balance}")
+
+balance1 = 1500
+balance2 = 3000
 
 atm1 = ATM(balance1, "Smart Bank")
 atm2 = ATM(balance2, "Baraka Bank")
 
 atm1.withdraw(277)
-atm1.withdraw(800)
+atm1.withdraw(100)
+atm1.withdraw(654)
+atm1.withdraw(55)
+atm1.withdraw(13)
+atm1.withdraw(174)
 
 atm2.withdraw(100)
-atm2.withdraw(2000)
+atm2.withdraw(1000)
+atm2.withdraw(65)
+atm2.withdraw(36)
+atm2.withdraw(87)
+atm2.withdraw(96)
+atm2.withdraw(125)
+atm2.withdraw(1255)
+
+atm1.show_withdrawals()
+atm2.show_withdrawals()
