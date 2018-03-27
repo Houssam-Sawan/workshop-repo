da716df86943f3caee6ee65cc4cf85af4da6bb89
Houssam Sawan <eng.houssam.sawan@gmail.com>
2018-03-27 09:55:47 +0300

    Create Models for Member and Post classes

---
+++ Forums/models.py
@@ -0,0 +1,23 @@
+class Member:
+    ''' Descripes a Forum's member '''
+
+    def __init__(self, name, age):
+        ''' Initialize new Member '''
+
+        self.name = name
+
+        self.age = age
+
+
+class Post:
+    ''' Descripes a Forum's post '''
+
+    def __init__(self, title, bodytxt):
+        ''' Initialize new Post '''
+        
+        self.title = title
+
+        self.txt = bodytxt
+
+
+
